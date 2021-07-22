455d6c89d52eaa6c7896d336bdfc19a69a9fff70
Grant <grantyli94@gmail.com>
2021-07-22 14:45:17 -0400

    user model tests

--- test_user_model.py
+++ test_user_model.py
@@ -8,6 +8,7 @@
 from app import app,  CURR_USER_KEY
 import os
 from unittest import TestCase
+from sqlalchemy.exc import IntegrityError
 
 from models import db, User, Message, Follows
 
@@ -17,6 +18,7 @@ from models import db, User, Message, Follows
 # connected to the database
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+PASSWORD = "HASHED_PASSWORD"
 
 # Now we can import app
 
@@ -26,11 +28,6 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 # and create fresh new clean test data
 
 db.create_all()
-testuser2 = {"username": "testuser2",
-             "email": "test2@test.com",
-             "password": "testuser2",
-             "image_url": None}
-
 
 class UserModelTestCase(TestCase):
     """Test views for messages."""
@@ -42,10 +39,11 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         Follows.query.delete()
 
-        u = User(
+        u = User.signup(
             email="test@test.com",
             username="testuser",
-            password="HASHED_PASSWORD"
+            password="HASHED_PASSWORD",
+            image_url=None
         )
 
         db.session.add(u)
@@ -108,7 +106,54 @@ class UserModelTestCase(TestCase):
                              test_follow.user_following_id)
 
             self.assertNotEqual(user3.id, test_follow.user_being_followed_id)
-            self.assertNotEqual(user3.id,  test_follow.user_following_id)
+            self.assertNotEqual(user3.id, test_follow.user_following_id)
+    
+    def test_user_signup(self):
+        """Successfully create a new user given valid credentials?"""
+
+        user = User.signup(username="testuser2",
+                         email="test2@test.com",
+                         password="testuser2",
+                         image_url = None)
+
+        db.session.add(user)
+        db.session.commit()
+
+        users = User.query.all()
+
+        self.assertEqual(len(users),2)
+
+        # signup(cls, username, email, password, image_url)
+
+    def test_user_invalid_signup(self):
+        """Test if invalid credentials fail to create a new user"""
+
+        with self.client as c:
+            resp = c.post('/signup', data={"username":"testuser",
+                            "email":"test2@test.com",
+                            "password":"testuser2"
+                            })
+
+            html = resp.get_data(as_text=True)
+            
+            users = User.query.all()
+            self.assertEqual(len(users),1)
+            self.assertIn('<li><a href="/signup">Sign up</a></li>', html)
+
+    def test_authentication(self):
+        """Test if valid user can be authenticated"""
+
+        user = User.authenticate(self.test_user1.username, PASSWORD)
+        self.assertEqual(self.test_user1, user)
+
+        user = User.authenticate(self.test_user1.username, "wrong_password")
+        self.assertIs(user, False)
+
+        user = User.authenticate("wrong_username", PASSWORD)
+        self.assertIs(user, False)
+
+
+        
 
     
     
