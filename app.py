47d6deabbe6dd316ecadb601713f4a894236a2da
Wukongthethird <simonzhang9518@gmail.com>
2024-07-21 18:32:12 -0700

    preparing to deploy to heroku

--- app.py
+++ app.py
@@ -10,17 +10,19 @@ from models import db, connect_db, User, Message, Like
 
 CURR_USER_KEY = "curr_user"
 
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
+
+database_url = database_url.replace('postgres://', 'postgresql://')
 app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'shh')
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
